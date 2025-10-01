76502709160795d8cc718cec5ba6b16e6eb28abc
asifahmed1810 <asifahmed18oct@gmail.com>
2025-10-01 18:05:38 +0600

    Added initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import { Outlet } from 'react-router'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1>This is home</h1>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+export default App
